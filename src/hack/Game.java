9476e3f299cfbf6da8f17e3cb7acfa1099f22787
jonyhay <joh00045@cs.stir.ac.uk>
2014-12-06 13:37:41 +0000

    Added instantiator

--- src/hack/Game.java
+++ src/hack/Game.java
@@ -1,13 +1,17 @@
 package hack;
 
 import java.awt.Graphics2D;
-
 import game2D.GameCore;
 
 @SuppressWarnings("serial")
 public class Game extends GameCore {
 	
-	public static void main(String[] args) { new Game(); }
+	public static void main(String[] args) { 
+		
+		Game game = new Game();
+		game.run(false, 640, 480);
+		
+	}
 
 	@Override
 	public void draw(Graphics2D g) {

6beef4b375cf85f0d48c693e3fb2d17efe7fb728
jonyhay <joh00045@cs.stir.ac.uk>
2014-12-06 13:31:04 +0000

    Added game2D lib

---
+++ src/hack/Game.java
@@ -0,0 +1,17 @@
+package hack;
+
+import java.awt.Graphics2D;
+
+import game2D.GameCore;
+
+@SuppressWarnings("serial")
+public class Game extends GameCore {
+	
+	public static void main(String[] args) { new Game(); }
+
+	@Override
+	public void draw(Graphics2D g) {
+		
+	}
+
+}

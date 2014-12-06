f4087e080092577dbe11636538a62794ada8076b
Skaiste <skb00007@students.stir.ac.uk>
2014-12-06 23:15:28 +0000

    zzz

--- src/hack/City.java
+++ src/hack/City.java
@@ -17,6 +17,9 @@ public class City extends Sprite {
 	private long countup = 0;
 
 	final private int maxInfection = 100; // max infection, elimination
+	
+	private int timer = 0;
+	final private int maxTimer = 50;
 
 	public City(String name, int population, int zombies, Animation a) {
 		super(a);
@@ -55,7 +58,7 @@ public class City extends Sprite {
 	}
 
 	public void updateInfection() {
-		if (zombies > 0)
+		if ((zombies > 0) && (timer == maxTimer))
 		{
 			int infection = (int) ((int) Math.random() * infectionRate + 1);
 			infection = population / maxInfection * infection;
@@ -67,7 +70,11 @@ public class City extends Sprite {
 	
 			if (zombies != 0)
 				increaseInfection();
+			
+			timer = 0;
 		}
+		else if (timer < maxTimer)
+			timer++;
 	}
 
 	public void eliminatingZombies(int eliminatingRate) {

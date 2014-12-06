ce8cbe21ca50812d0c23777408295c44fa980fd0
Daniel <Daniel@192.168.1.160>
2014-12-06 14:03:19 +0000

    Added agent stufff

---
+++ AgentHandler.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class AgentHandler 
+{
+	private ArrayList<E> agentList<Agent>;
+	
+	public AgentHandler()
+	{
+
+	}
+	
+	public ArrayList<Agent> getAgentList()
+	{
+		return agentList;
+	}
+	
+	public void addAgent(agent Agent)
+	{
+		agentList.add(agent);
+	}
+	
+	public void remove agent(agent Agent)
+	{
+		agentList.remove(agent);
+	}
+}

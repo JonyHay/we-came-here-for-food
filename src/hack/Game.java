f3e7064a86678f3c7120e71f27b142c4796f2f11
jonyhay <joh00045@cs.stir.ac.uk>
2014-12-06 15:54:48 +0000

    Scores

--- src/hack/Game.java
+++ src/hack/Game.java
@@ -1,7 +1,11 @@
 package hack;
 
 import java.awt.Graphics2D;
+import java.awt.Toolkit;
+
+import game2D.Animation;
 import game2D.GameCore;
+import game2D.Sprite;
 
 @SuppressWarnings("serial")
 public class Game extends GameCore {
@@ -9,12 +13,20 @@ public class Game extends GameCore {
 	public static void main(String[] args) { 
 		
 		Game game = new Game();
+		game.init();
 		game.run(false, 640, 480);
 		
 	}
-
+	
+	public void init() {
+		
+		
+		
+	}
+  
 	@Override
 	public void draw(Graphics2D g) {
+		
 		
 	}
 
---
+++ src/hack/Scoreboard.java
@@ -0,0 +1,25 @@
+package hack;
+
+import java.util.ArrayList;
+
+public class Scoreboard {
+	
+	private ArrayList<Score> scores;
+	
+	public Scoreboard() {
+		
+		scores = new ArrayList<Score>();
+		
+	}
+	
+	public Score[] getScores() {
+		return null;
+		
+		
+	}
+	
+	public void sort() {
+		
+	}
+
+}
